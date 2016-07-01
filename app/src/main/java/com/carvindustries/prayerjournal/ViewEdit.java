33fac14c4721a37187970f819e733ede6f09d1e3
rmainard007 <9FYC@!9a>
2016-06-30 17:54:05 -0700

    ListView touch item event save. Crashes the app, but it's a start

--- app/src/main/java/com/carvindustries/prayerjournal/ViewEdit.java
+++ app/src/main/java/com/carvindustries/prayerjournal/ViewEdit.java
@@ -1,10 +1,15 @@
 package com.carvindustries.prayerjournal;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
@@ -13,6 +18,7 @@ import java.util.List;
 
 public class ViewEdit extends AppCompatActivity {
     DataBase prayers;
+    EditText result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,17 +29,53 @@ public class ViewEdit extends AppCompatActivity {
     private void addListView() {
         List<PrayerEntry> plistPassedIn = prayers.getAllPrayer();
         //this needs som work
-        ArrayAdapter<PrayerEntry> prayerlist = new ArrayAdapter<PrayerEntry>(this, android.R.layout.simple_list_item_1,plistPassedIn);
+        final ArrayAdapter<PrayerEntry> prayerlist = new ArrayAdapter<PrayerEntry>(this, android.R.layout.simple_list_item_1,plistPassedIn);
         ListView plist = (ListView)findViewById(R.id.prayerlist);
         plist.setAdapter(prayerlist);
 
         plist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // call a sub activity that allows the edting of the text then saves to the db
-                // with a new date but the same id.
+                result = null;
+                PrayerEntry editItem = (PrayerEntry) parent.getItemAtPosition(position);
+                LayoutInflater dlgSv = LayoutInflater.from(ViewEdit.this);
+                View dialogSaveView = dlgSv.inflate(R.layout.edit_prayer, null);
+                AlertDialog.Builder savePrayerBuilder = new AlertDialog.Builder(ViewEdit.this);
+
+                savePrayerBuilder.setView(dialogSaveView);
+
+                final EditText userInput = (EditText) dialogSaveView.findViewById(R.id.saveDialogEntry);
+
+                savePrayerBuilder
+                        .setCancelable(true)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener(){
+
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+
+                                        result.setText(userInput.getText());
+                                    }
+
+                                })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                if(result!=null){
+                    editItem.setText(editItem.getText() + "\n" + result.getText().toString());
+                    prayers.editPrayerEntry(editItem.getPid(), editItem.getText());
+                    prayerlist.notifyDataSetChanged();
+                }
+                AlertDialog showEditor = savePrayerBuilder.create();
+                showEditor.show();
+
             }
         });
     }
+
 
 }
